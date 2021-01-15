deba20660e336adff4bd8808aab1be9046a7fb9e
Pavel Kachan <asis22@users.noreply.github.com>
2021-01-15 10:43:05 +0700

    first

---
+++ Potomok.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Potomok: IComparable
+    {
+        public double X;
+        
+        public double Y;
+
+        public int Compare(Potomok x, Potomok y)
+        {
+            var dlt = x.Y - y.Y;
+            if (dlt > 0)
+                return 1;
+            if (dlt < 0)
+                return -1;
+            return 0;
+        }
+
+        public int CompareTo(object obj)
+        {
+            var y = obj as Potomok;
+            var dlt = Y - y.Y;
+            if (dlt > 0)
+                return 1;
+            if (dlt < 0)
+                return -1;
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static GenAlg GA = new GenAlg();
+
+        static void Main(string[] args)
+        {
+            GA.Func = (x) => { return x * x + 4; };
+            GA.Init();
+            var result = GA.Process();
+            Console.WriteLine("iter: {0}", GA.wag);
+            Console.WriteLine("Rezyltat: x = {0}; y = {1}.", result.X, result.Y);
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
